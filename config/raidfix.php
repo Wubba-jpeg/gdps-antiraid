b6b5c4340a1673f8212689865b7647af6f653591
Wubba-jpeg <Pdxfxa@gmail.com>
2026-07-22 19:33:18 +0100

    Update raidfix.php

--- config/raidfix.php
+++ config/raidfix.php
@@ -1,5 +1,5 @@
 <?php
 // config for the raidfix
 $levelperhr = 10; /* here, put the amount of levels you want to allow each hour before requests are denied */
-$starsbuffer = 50; /* when submitting your stars, if your stars are above the max amount of stars one can possibly gain, you're blocked. however, a buffer is here for dailies / weeklies / events, etc. (if you're even using this for 1.9+). */
+$starsbuffer = 200; /* when submitting your stars, if your stars are above the max amount of stars one can possibly gain, you're blocked. however, a buffer is here for dailies / weeklies / events, etc. (if you're even using this for 1.9+). */
 // I'll (soon) add more choices here. if you want anything to be added, make an issue in the repo
